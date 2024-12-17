22b4a444c025f65241ed1f24ab2adf8f0f289275
Stephen Goncalves <stiefo.o@gmail.com>
2024-12-17 19:06:14 +0000

    initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/components/**/*.{ts,tsx}",
+    "./src/app/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config;
